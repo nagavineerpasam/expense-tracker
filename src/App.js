7c0367425f18beaa01744024ff701ebc990a3ba8
nagavineerpasam <nagavineerpasam@gmail.com>
2020-07-09 22:29:33 +0530

    what

--- src/App.js
+++ src/App.js
@@ -1,65 +1,26 @@
 import React from 'react';
-import Todo from './Todo';
-export default class App extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            items : [],
-            currentItem : {
-                text : '',
-                key : ''
-            }
-        }
-        this.handleInput = this.handleInput.bind(this);
-        this.addItem = this.addItem.bind(this);
-        this.deleteItem = this.deleteItem.bind(this);
-    }
-    addItem(e){
-        e.preventDefault();
-        const newItem = this.state.currentItem;
-        if(newItem.text!==""){
-        const items = [...this.state.items, newItem];
-        
-        this.setState({
-           items : items,
-           currentItem : {
-               text : '',
-               key : ''
-           }
-        })
-        
-    }
-}
-    deleteItem(key){
-        const newItems = this.state.items.filter(item => item.key !== key);
-        this.setState({
-            items : newItems
-        })
-    }
-    handleInput(e){
-        this.setState({
-            currentItem :{
-                text : e.target.value,
-                key : Date.now()
-            }
-        })
-    }
-    
-    render(){
-        return (
-            <div className="container">
-            <div className="App">
-                <h1 ><i className="fa fa-fire" style={{color:"orange",marginRight:20}}>
-                </i>TODO APP</h1><br/><br/>
-                <form onSubmit={this.addItem}>
-                <input style={{marginRight:20}} type="text" placeholder="add here..." 
-                value={this.state.currentItem.text} onChange = {this.handleInput}/>
-                <button type="submit" className="btn btn-warning">ADD</button>
-                </form><br/>
-                <Todo items={this.state.items} deleteItem={this.deleteItem}/>
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
 
-            </div>
-            </div>
-        )
-    }
+import { GlobalProvider } from './context/GlobalState';
+
+import './App.css';
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
 }
+
+export default App;
